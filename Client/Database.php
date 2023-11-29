bd360e43806b11faa22ce5d49cc002db9173c9da
Migzly22 <rolly.migrino02@gmail.com>
2023-11-29 17:09:33 +0800

    update

--- Client/Database.php
+++ Client/Database.php
@@ -2,19 +2,20 @@
     error_reporting(E_ERROR | E_PARSE);
 //error_reporting(0);
 
-/*
 //error_reporting(0);
 $localhost = "localhost";
 $username = "u970357751_elijosh_db";
 $pass = "sDau1HE3M[";
 $dbname = "u970357751_elijoshresort";
 //error_reporting(0);
-*/
 
+
+/*
 $localhost = "localhost";
 $username = "root";
 $pass = "";
 $dbname = "elijoshresort";
+*/
 
 
 $conn = mysqli_connect($localhost,$username,$pass,$dbname);
