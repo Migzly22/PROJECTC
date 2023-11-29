7760fc9cfe9ddc4e9a554b21cf3cfd9ec101beba
Migzly22 <rolly.migrino02@gmail.com>
2023-11-29 18:32:14 +0800

    still fixing 1

--- Client/ResetPass.php
+++ Client/ResetPass.php
@@ -10,7 +10,7 @@
     <script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
-    <script src="./JS/resetpass.js" defer></script>
+    <script src="./JS/resetpass1.js" defer></script>
 </head>
 <body>
     <section>
