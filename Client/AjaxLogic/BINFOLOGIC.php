deb29576b668788bbe4e3ce2c728ca9ccd96e1b3
Migzly22 <rolly.migrino02@gmail.com>
2023-12-02 23:56:28 +0800

    fix logic update in reload

--- Client/AjaxLogic/BINFOLOGIC.php
+++ Client/AjaxLogic/BINFOLOGIC.php
@@ -6,7 +6,7 @@ ob_start();
 
 $sqlcode = $_POST["sqlcode"];
 
-$sqlcode3 = "SELECT  a.*, b.*, IF(b.Description = 'Pending', 'Pending', 'Done') AS STATUSHEHE FROM reservations a LEFT JOIN guestpayments b ON a.ReservationID = b.ReservationID WHERE a.UserID = 16 AND b.Description is NOT NULL ORDER BY a.ReservationID DESC;";
+$sqlcode3 = "SELECT  a.*, b.*, IF(b.Description = 'Pending', 'Pending', 'Done') AS STATUSHEHE FROM reservations a LEFT JOIN guestpayments b ON a.ReservationID = b.ReservationID WHERE a.UserID = ".$_SESSION["USERID"]." AND b.Description is NOT NULL ORDER BY a.ReservationID DESC;";
 
 
 function PRINTING($conn, $sqlcode3){
