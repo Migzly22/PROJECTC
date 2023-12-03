93b00f4a92b0c11d5cff42ccb8ef507f60e61e4a
Migzly22 <rolly.migrino02@gmail.com>
2023-12-03 09:36:14 +0800

    send

--- Admins/AjaxLogic/LOGOUTLOGIC.php
+++ Admins/AjaxLogic/LOGOUTLOGIC.php
@@ -5,13 +5,13 @@ ob_start();
 
     if (isset($_SESSION["USERID"]) || isset($_SESSION["ACCESS"])){
         session_destroy();
-        header("Location: ../Client/index.php");
+        header("Location: https://elijoshresortandeventsplace.com/");
         ob_end_flush();
         exit();
 
     }else{
           // If the user is not logged in, redirect to the login page
-          header("Location: ../Client/index.php");
+          header("Location: https://elijoshresortandeventsplace.com/");
           ob_end_flush();
           exit();
     }
