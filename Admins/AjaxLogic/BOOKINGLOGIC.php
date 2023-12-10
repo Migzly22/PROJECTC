4bef7fae906055472825e7572218c7e82e3e3fea
Migzly22 <rolly.migrino02@gmail.com>
2023-12-10 21:01:52 +0800

    booking

--- Admins/AjaxLogic/BOOKINGLOGIC.php
+++ Admins/AjaxLogic/BOOKINGLOGIC.php
@@ -15,31 +15,65 @@ function PRINTING($conn, $sqlcode3){
     $table5 = "";
 
     while ($result = mysqli_fetch_assoc($querynum3)) {
+        $RESERVATIONDETAILS = "
+        <td scope='col' >
+            ".$result['ReservationStatus']."
+            <select>
+                <option value='BOOKED'>Booked</option>
+                <option value='CHECKIN'>Check-in</option>
+                <option value='CANCELLED'>Cancelled</option>
+            </select>
+        </td>";
+        $select1 = "";$select2 = "";$select2 = "";
+        switch ($result['ReservationStatus']) {
+            case 'BOOKED':
+                $select1 = "selected";
+                break;
+            case 'CHECKIN':
+                $select2 = "selected";
+                break;
+            case 'CANCELLED':
+                $select3 = "selected";
+                break;
+        }
+        $RESERVATIONDETAILS = "
+        <td scope='col' >
+            <div class='ACCESSTABLE'>
+                <select  onchange='CHANGESTATE(this,`".$result['ReservationID']."`)'>
+                    <option value='BOOKED' $select1>Booked</option>
+                    <option value='CHECKIN' $select2>Check-in</option>
+                    <option value='CANCELLED' $select3>Cancelled</option>
+                </select>                   
+            </div>
+        </td>";
+
+        if($result['ReservationStatus'] == "CHECKOUT"){
+            $RESERVATIONDETAILS = "<td scope='col' >Check out</td>";
+        }
+
         # code...
         $table5 .= "
-            <tr>
-                <td>".$result['Name']."</td>
-                <td>".$result['Email']."</td>
-                <td scope='col' style='text-align: center;'>".$result['CheckInDate']."</td>
-                <td scope='col' style='text-align: center;'>".$result['CheckOutDate']."</td>
-                <td scope='col' style='text-align: end;'>".$result['TotalPrice']."</td>
-                <td scope='col' style='text-align: end;'>".$result['Downpayment']."</td>
-                <td scope='col'>".$result['ReservationStatus']."</td>
-                <td class='ActionTABLE' id='".$result['ReservationID']."'>
-                    <button class='addbtn' onclick='VIEW(`".$result['ReservationStatus']."`,`".$result['GuestID']."`)'>
-                        <svg xmlns='http://www.w3.org/2000/svg' height='1em' viewBox='0 0 576 512'><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d='M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z'/></svg>
-                    </button>
-                    <button class='Editbtn' onclick='EDIT(`".$result['ReservationStatus']."`,this)'>
-                        <svg xmlns='http://www.w3.org/2000/svg' height='1em' viewBox='0 0 512 512'><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d='M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z'/></svg>
-                    </button>
-                </td>
-        </tr>
-        ";
+        <tr>
+            <td>".$result['Name']."</td>
+            <td>".$result['Email']."</td>
+            <td scope='col' style='text-align: center;'>".$result['eCheckin']."</td>
+            <td scope='col' style='text-align: center;'>".$result['finalCheckout']."</td>
+            $RESERVATIONDETAILS
+            <td class='ActionTABLE' id='".$result['ReservationID']."'>
+                <button class='addbtn' onclick='VIEW(`".$result['ReservationStatus']."`,`".$result['GuestID']."`)'>
+                    <svg xmlns='http://www.w3.org/2000/svg' height='1em' viewBox='0 0 576 512'><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d='M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z'/></svg>
+                </button>
+                <button class='addbtn' onclick='PAYMENT(`".$result['ReservationID']."`)'>
+                <svg xmlns='http://www.w3.org/2000/svg' height='1em' viewBox='0 0 512 512'><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d='M64 0C46.3 0 32 14.3 32 32V96c0 17.7 14.3 32 32 32h80v32H87c-31.6 0-58.5 23.1-63.3 54.4L1.1 364.1C.4 368.8 0 373.6 0 378.4V448c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V378.4c0-4.8-.4-9.6-1.1-14.4L488.2 214.4C483.5 183.1 456.6 160 425 160H208V128h80c17.7 0 32-14.3 32-32V32c0-17.7-14.3-32-32-32H64zM96 48H256c8.8 0 16 7.2 16 16s-7.2 16-16 16H96c-8.8 0-16-7.2-16-16s7.2-16 16-16zM64 432c0-8.8 7.2-16 16-16H432c8.8 0 16 7.2 16 16s-7.2 16-16 16H80c-8.8 0-16-7.2-16-16zm48-168a24 24 0 1 1 0-48 24 24 0 1 1 0 48zm120-24a24 24 0 1 1 -48 0 24 24 0 1 1 48 0zM160 344a24 24 0 1 1 0-48 24 24 0 1 1 0 48zM328 240a24 24 0 1 1 -48 0 24 24 0 1 1 48 0zM256 344a24 24 0 1 1 0-48 24 24 0 1 1 0 48zM424 240a24 24 0 1 1 -48 0 24 24 0 1 1 48 0zM352 344a24 24 0 1 1 0-48 24 24 0 1 1 0 48z'/></svg>
+            </button>
+            </td>
+    </tr>
+    ";
     }
 
     if (mysqli_num_rows($querynum3) == 0) {
         $table5 = "     <tr>
-            <td colspan='7' style='text-align:center; font-weight:bolder;'>No data </td>
+            <td colspan='6' style='text-align:center; font-weight:bolder;'>No data </td>
         </tr> ";
     }
     echo $table5;
--- Admins/CUKXGXITKQ/booking-123.php
+++ Admins/CUKXGXITKQ/booking-123.php
@@ -34,71 +34,36 @@
                                 <tr>
                                     <th scope='col'>Name</th>
                                     <th scope='col'>Email</th>
-                                    <th scope='col' style="text-align: center;">Reservation</th>
-                                    <th scope='col' style="text-align: center;">Checkout</th>
-                                    <th scope='col' style="text-align: center;">Status</th>
+                                    <th scope='col'>Reservation</th>
+                                    <th scope='col'>Checkout</th>
+                                    <th scope='col'>Price</th>
+                                    <th scope='col'>Downpament</th>
+                                    <th scope='col'>Status</th>
                                     <th scope='col' style="text-align: center;">Action</th>
                                 </tr>
                             </thead>
                             <tbody id="TBODYELEMENT">
-                               
 <?php
-    $sqlbooking = "SELECT a.*, b.*, CONCAT(b.LastName,', ', b.FirstName) as Name FROM reservations a LEFT JOIN guests b ON a.GuestID = b.GuestID AND a.ReservationStatus != 'CANCELLED' ORDER BY a.CheckInDate DESC;";
+    $sqlbooking = "SELECT a.*, b.*, CONCAT(b.LastName,', ', b.FirstName) as Name FROM reservations a LEFT JOIN guests b ON a.GuestID = b.GuestID ORDER BY a.CheckInDate DESC;";
     $querybooking = mysqli_query($conn,$sqlbooking);
     $tbodydata = "";
     while ($result = mysqli_fetch_assoc($querybooking)) {
         # code...
-
-
-        $RESERVATIONDETAILS = "
-        <td scope='col' >
-            ".$result['ReservationStatus']."
-            <select>
-                <option value='BOOKED'>Booked</option>
-                <option value='CHECKIN'>Check-in</option>
-                <option value='CANCELLED'>Cancelled</option>
-            </select>
-        </td>";
-        $select1 = "";$select2 = "";$select2 = "";
-        switch ($result['ReservationStatus']) {
-            case 'BOOKED':
-                $select1 = "selected";
-                break;
-            case 'CHECKIN':
-                $select2 = "selected";
-                break;
-            case 'CANCELLED':
-                $select3 = "selected";
-                break;
-        }
-        $RESERVATIONDETAILS = "
-        <td scope='col' >
-            <div class='ACCESSTABLE'>
-                <select  onchange='CHANGESTATE(this,`".$result['ReservationID']."`)'>
-                    <option value='BOOKED' $select1>Booked</option>
-                    <option value='CHECKIN' $select2>Check-in</option>
-                    <option value='CANCELLED' $select3>Cancelled</option>
-                </select>                   
-            </div>
-        </td>";
-
-        if($result['ReservationStatus'] == "CHECKOUT"){
-            $RESERVATIONDETAILS = "<td scope='col' >Check out</td>";
-        }
-
         $tbodydata .= "
             <tr>
                 <td>".$result['Name']."</td>
                 <td>".$result['Email']."</td>
-                <td scope='col' style='text-align: center;'>".$result['eCheckin']."</td>
-                <td scope='col' style='text-align: center;'>".$result['finalCheckout']."</td>
-                $RESERVATIONDETAILS
+                <td scope='col' style='text-align: center;'>".$result['CheckInDate']."</td>
+                <td scope='col' style='text-align: center;'>".$result['CheckOutDate']."</td>
+                <td scope='col' style='text-align: end;'>".$result['TotalPrice']."</td>
+                <td scope='col' style='text-align: end;'>".$result['Downpayment']."</td>
+                <td scope='col' >".$result['ReservationStatus']."</td>
                 <td class='ActionTABLE' id='".$result['ReservationID']."'>
                     <button class='addbtn' onclick='VIEW(`".$result['ReservationStatus']."`,`".$result['GuestID']."`)'>
                         <svg xmlns='http://www.w3.org/2000/svg' height='1em' viewBox='0 0 576 512'><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d='M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z'/></svg>
                     </button>
-                    <button class='addbtn' onclick='PAYMENT(`".$result['ReservationID']."`)'>
-                        <svg xmlns='http://www.w3.org/2000/svg' height='1em' viewBox='0 0 512 512'><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d='M64 0C46.3 0 32 14.3 32 32V96c0 17.7 14.3 32 32 32h80v32H87c-31.6 0-58.5 23.1-63.3 54.4L1.1 364.1C.4 368.8 0 373.6 0 378.4V448c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V378.4c0-4.8-.4-9.6-1.1-14.4L488.2 214.4C483.5 183.1 456.6 160 425 160H208V128h80c17.7 0 32-14.3 32-32V32c0-17.7-14.3-32-32-32H64zM96 48H256c8.8 0 16 7.2 16 16s-7.2 16-16 16H96c-8.8 0-16-7.2-16-16s7.2-16 16-16zM64 432c0-8.8 7.2-16 16-16H432c8.8 0 16 7.2 16 16s-7.2 16-16 16H80c-8.8 0-16-7.2-16-16zm48-168a24 24 0 1 1 0-48 24 24 0 1 1 0 48zm120-24a24 24 0 1 1 -48 0 24 24 0 1 1 48 0zM160 344a24 24 0 1 1 0-48 24 24 0 1 1 0 48zM328 240a24 24 0 1 1 -48 0 24 24 0 1 1 48 0zM256 344a24 24 0 1 1 0-48 24 24 0 1 1 0 48zM424 240a24 24 0 1 1 -48 0 24 24 0 1 1 48 0zM352 344a24 24 0 1 1 0-48 24 24 0 1 1 0 48z'/></svg>
+                    <button class='Editbtn' onclick='EDIT(`".$result['ReservationStatus']."`,this)'>
+                        <svg xmlns='http://www.w3.org/2000/svg' height='1em' viewBox='0 0 512 512'><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d='M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z'/></svg>
                     </button>
                 </td>
         </tr>
@@ -133,6 +98,8 @@
         let searchcondition = `(
             a.ReservationID LIKE '%${item}%' OR
             a.GuestID LIKE '%${item}%' OR
+            a.RoomNumber LIKE '%${item}%' OR
+            a.CottageTypeID LIKE '%${item}%' OR
             a.ReservationStatus LIKE '%${item}%' OR
             b.FirstName LIKE '%${item}%' OR
             b.LastName LIKE '%${item}%' OR
@@ -214,15 +181,52 @@
 
         }
     }
-    async function CHANGESTATE(e,id){
-        let sqlcode123 =`UPDATE reservations SET ReservationStatus = '${e.value}' WHERE ReservationID = '${id}';`
-        const Tabledata =await AjaxSendv3(sqlcode123,"BOOKINGLOGIC","&Process=AccessUpdate")
-        TBODYELEMENT.innerHTML = Tabledata
-    }
+    
     async function VIEW(status,e){
         location.href = `./Mainpage.php?nzlz=reservationview&plk=2&ISU=${e}`;
     }
+    async function EDIT(status,e){
+        let targetid = e.parentNode.id
 
+        let statuscheck = ["","","","",];
+        switch (status) {
+            case 'BOOKED':
+                statuscheck[0] = "selected"
+                break;
+            case 'CHECKIN':
+                statuscheck[1] = "selected"
+                break;
+            case 'CHECKOUT':
+                statuscheck[2] = "selected"
+                break;
+            case 'CANCELLED':
+                statuscheck[3] = "selected"
+                break;
+        }
+
+        let design = `
+        <div class='sweetDIVBOX'>
+            <div class='SWEETFORMS'>
+                <label for='swal-input1'>Reservation Status</label>
+                <select class='SWALinput swalselect' id='swal-input1' aria-label='Floating label select example'>
+                    <option value='BOOKED' ${statuscheck[0]}>Booked</option>
+                    <option value='CHECKIN' ${statuscheck[1]}>Checked in</option>
+                    <option value='CHECKOUT' ${statuscheck[2]}>Checked out</option>
+                    <option value='CANCELLED' ${statuscheck[3]}>Cancelled</option>
+                </select>
+            </div>
+        </div>`
+
+        let formValues =await POPUPCREATE("Filters",design,1)
+
+        if (formValues) {
+            let sqlcode123 =`UPDATE reservations SET ReservationStatus = '${formValues[0]}' WHERE ReservationID = '${targetid}';`
+                console.log(sqlcode123)
+            const Tabledata =await AjaxSendv3(sqlcode123,"BOOKINGLOGIC","&Process=AccessUpdate")
+            TBODYELEMENT.innerHTML = Tabledata
+
+        }
+    }
 
     async function ADDSTAFF(){
         let design = `
@@ -279,64 +283,5 @@
 
 
         //location.href = `./Mainpage.php?nzlz=reservationform&plk=2&cin=2023-12-10&ETIME=12:00&adultval=200&kidval=150&package=Package1&tRANGE=Day&na=4&nk=3&ns=3&tinit=1730.00`;
-    }
-
-    async function PAYMENT(e){
-
-        let balance = await AjaxSendv3(e,"GUESTLOGIC","&Process=Specialmention")
-        console.log(balance)
-        //location.href = `./Mainpage.php?nzlz=reservationform&plk=2`;
-
-        let design =  `
-        <div class='sweetDIVBOX'>
-            <div class='SWEETFORMS'>
-                <label for='swal-input3'>Balance</label>
-                <input type ="text" id="swal-input3" class="SWALinput" readonly value='${balance}'>
-            </div>
-            <div class='SWEETFORMS'>
-                <label for='swal-input1'>Payment</label>
-                <input type ="text" id="swal-input1" class="SWALinput" required>
-            </div>
-            <div class='SWEETFORMS'>
-                <label for='swal-input2'>Type of Payment</label>
-                <select class='SWALinput swalselect' id='swal-input2' aria-label='Floating label select example'>
-                    <option value='CASH'>Cash</option>
-                    <option value='ONLINE'>Online Payment</option>
-                </select>
-            </div>
-        </div>`
-
-        let formValues =await POPUPCREATE("Checkout Payment",design,3)
-
-        if (formValues) {
-            let conditions = [];
-
-            conditions.push(`${formValues[1]}`);
-
-            if(formValues[0] !== "" && parseFloat(formValues[0]) >= parseFloat(balance)){
-                conditions.push(`${balance}`);
-                let sqlcodepayment = `INSERT INTO guestpayments ( ReservationID, PaymentDate, AmountPaid, PaymentMethod, Description) VALUES ('${e}', CURRENT_DATE , '${conditions[1]}', '${conditions[0]}', 'CHECKOUT');`;
-                await AjaxSendv3(sqlcodepayment,"GUESTLOGIC",`&Process=Insertmore`)
-                
-                let update12 =`UPDATE reservations SET ReservationStatus = 'CHECKOUT' WHERE ReservationID = '${e}';`
-                await AjaxSendv3(update12,"GUESTLOGIC","&Process=Insertmore")
-
-
-                await Swal.fire({
-                    title: "",
-                    text: `Change : ₱ ${(parseFloat(formValues[0]) - parseFloat(balance)).toFixed(2)}`,
-                    icon: "info"
-                });
-                RESETTABLE()
-            }else{
-                await Swal.fire({
-                    title: "",
-                    text: "Wrong Payment Value",
-                    icon: "warning"
-                });
-            }
-
-            
-        }
     }
 </script>
