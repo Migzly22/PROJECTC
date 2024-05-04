741b28bc988697ac726c5f226ac1e9fbe3e131a1
Rolly Migriño <rolly.migrino02@gmail.com>
2024-05-04 12:11:09 +0800

    Update new

--- EliJosh_Special/specialcon.php
+++ EliJosh_Special/specialcon.php
@@ -40,7 +40,7 @@ $targetlinks = isset($_GET["nzlz"]) ? $_GET["nzlz"] : "booking";
 	<title>Admin | EliJosh Resort & Events Place</title>
 	<link rel="icon" type="image/x-icon" href="./img/title_logo.ico">
 
-	<script src="https://www.paypal.com/sdk/js?client-id=AT5hayGPfCenF8pOB0oMvKEXD67u_BrLvgg6hZ6xX7VdwygRQNnd2-5vza2rYScl5uTAcRrhp_VWFlqw"></script>
+	<script src="https://www.paypal.com/sdk/js?client-id=AQQCXHUkMB4x4jEOsIgAgWrhWUbBIWXuUXQjgaijUKzG_6a3EBS6hk5Y0yoIVhlEdY6OOmSzBjIHpYbb"></script>
 </head>
 <style>
 	/* Override default styles for the textarea */
